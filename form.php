67ff9edbcf40af5f0a51d31cc47848ad9c71f05a
Thijs Van der Schaeghe <thijs@catlab.be>
2019-12-15 00:53:22 +0100

    Initial commit

---
+++ form.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Download video</title>
+</head>
+<body>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+<div class="container">
+
+    <h1>Download</h1>
+    <form method="post">
+        <div class="form-group">
+            <label for="url">Video URL</label>
+            <input type="url" class="form-control" id="url" name="url" placeholder="URL">
+        </div>
+
+        <div class="form-group">
+            <label for="name">Output file name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+        </div>
+
+        <div class="form-group">
+            <label for="name">Convert to</label>
+            <select class="form-control" id="downloadType" name="downloadType">
+                <option value="audio">Audio only</option>
+                <option value="video">Video</option>
+                <option value="video-only">Video only</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="name">Skip the first seconds</label>
+            <input type="number" class="form-control" id="skipTo" name="skipTo" placeholder="Skip first seconds">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Download</button>
+    </form>
+
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,150 @@
+<?php
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+define('TMP_DOWNLOAD_DIR', '/tmp/youtube-dl/');
+
+use FFMpeg\Filters\Video\VideoFilterInterface;
+use FFMpeg\Format\VideoInterface;
+use FFMpeg\Media\Video;
+use YoutubeDl\YoutubeDl;
+use YoutubeDl\Exception\CopyrightException;
+use YoutubeDl\Exception\NotFoundException;
+use YoutubeDl\Exception\PrivateVideoException;
+
+if (!isset($_POST['url'])) {
+    include 'form.php';
+    exit;
+}
+
+// For more options go to https://github.com/rg3/youtube-dl#user-content-options
+$url = isset($_POST['url']) ? $_POST['url'] : null;
+$type = isset($_POST['downloadType']) ? $_POST['downloadType'] : 'video';
+$skip = isset($_POST['skipTo']) ? $_POST['skipTo'] : 0;
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+//$destination = '/home/daedeloth/Team Drives/thijs@catlab.be/De Quizfabriek/Quizzes/Edities/Seizoen 5/QW 5.3 Radio Quizfabriek (muziekquiz)/Attachments/Gedownload';
+//$destination = '/home/daedeloth/Fragmenten/';
+
+if (!file_exists(TMP_DOWNLOAD_DIR)) {
+    mkdir(TMP_DOWNLOAD_DIR);
+}
+
+
+class VideoMuteFilter implements VideoFilterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(Video $video, VideoInterface $format)
+    {
+        return array('-an');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPriority()
+    {
+        return 1;
+    }
+}
+
+$createdFiles = [];
+
+try {
+    $options = [
+        'continue' => true, // force resume of partially downloaded files. By default, youtube-dl will resume downloads if possible.
+    ];
+
+    if ($type === 'audio') {
+        $options['extract-audio'] = true;
+        $options['audio-format'] = 'mp3';
+        $options['audio-quality'] = 0;
+        $options['output'] = '%(title)s.%(ext)s';
+    }
+
+    $dl = new YoutubeDl($options);
+    $dl->setDownloadPath(TMP_DOWNLOAD_DIR);
+
+    $video = $dl->download($url);
+    $filename = $video->getFilename();
+
+    $extension = explode('.', $filename);
+    $extension = $extension[count($extension) - 1];
+
+    $currentFilename = TMP_DOWNLOAD_DIR . $filename;
+    $createdFiles[] = $currentFilename;
+
+    //echo '<pre>';
+    //echo 'Downloaded ' . $filename . " to " . $currentFilename . "\n";
+
+    // now cut
+    $ffmpeg = FFMpeg\FFMpeg::create([
+        'timeout'          => 3600, // The timeout for the underlying process
+        'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+    ]);
+
+    $finalVideo = $ffmpeg->open($currentFilename);
+
+    $clipStart = $skip;
+    $clipDuration = 120;
+
+    $start = FFMpeg\Coordinate\TimeCode::fromSeconds($clipStart);
+    $duration = FFMpeg\Coordinate\TimeCode::fromSeconds($clipDuration);
+
+    $finalVideo->filters()->clip($start, $duration);
+
+    // video or audio?
+    if ($type === 'audio') {
+        $format = new \FFMpeg\Format\Audio\Mp3();
+        $extension = 'mp3';
+    } else {
+        $format = new FFMpeg\Format\Video\X264('aac');
+        $extension = 'mp4';
+    }
+
+    if ($type === 'video-only') {
+        $finalVideo->addFilter(new VideoMuteFilter());
+    }
+
+    if ($clipStart > 0 && $type === 'audio') {
+        $finalVideo->filters()->custom('afade=t=in:st=' . $clipStart . ':d=1');
+    }
+
+    $finalDestination = TMP_DOWNLOAD_DIR . '/' . tmpfile() . '.' . $extension;
+    //echo 'Saving to ' . $finalDestination . "\n";
+
+    $finalVideo->save($format, $finalDestination);
+    $createdFiles[] = $finalDestination;
+
+    //echo 'Saved to ' . $finalDestination . "\n";
+
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary");
+    header("Content-disposition: attachment; filename=\"" . $name . '.' . $extension . "\"");
+
+    readfile($finalDestination);
+
+    // $video->getFile(); // \SplFileInfo instance of downloaded file
+} catch (NotFoundException $e) {
+    // Video not found
+    echo '<pre>';
+    echo $e;
+} catch (PrivateVideoException $e) {
+    // Video is private
+    echo '<pre>';
+    echo $e;
+} catch (CopyrightException $e) {
+    // The YouTube account associated with this video has been terminated due to multiple third-party notifications of copyright infringement
+    echo '<pre>';
+    echo $e;
+} catch (\Exception $e) {
+    // Failed to download
+    echo '<pre>';
+    echo $e;
+}
+
+foreach ($createdFiles as $v) {
+    unlink($v);
+}
