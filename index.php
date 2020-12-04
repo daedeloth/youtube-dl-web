a8aee100f4bffe7edb97481c4b2624178fbfcb5d
Thijs Van der Schaeghe <thijs@catlab.be>
2020-12-04 21:35:22 +0100

    Remove duration limitation.

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ try {
     // now cut
     $ffmpeg = FFMpeg\FFMpeg::create([
         'timeout'          => 3600, // The timeout for the underlying process
-        'ffmpeg.threads'   => 4,   // The number of threads that FFMpeg should use
+        'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
     ]);
 
     $finalVideo = $ffmpeg->open($currentFilename);
